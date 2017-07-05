616f9ee3bb3ebab2fb2733e23a0feddad5e6cc5b
Plamen Andonov <plamen.p.andonov@gmail.com>
2017-07-05 23:11:56 +0100

    Tower and Buildings task

--- js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('#site-menu-btn').on('click', function(){
-        $('body').addClass('active-navigation');
-    });
-    $('#site-menu').on('click', function(){
-        $('body').removeClass('active-navigation');
-    });
-});
